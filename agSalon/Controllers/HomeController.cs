a41967bad79dd12ea43e9e78f86cbe193673266f
Hanna Huba <gubanna.11@gmail.com>
2023-03-19 16:21:27 +0200

    Initial

---
+++ agSalon/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using agSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace agSalon.Controllers
+{
+	public class HomeController : Controller
+	{
+
+		public HomeController()
+		{
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+	}
+}
